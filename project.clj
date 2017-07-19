24fb97cec20217715a172404458a81bc0d5aed59
zhangbaoliang <zhangbaoliang.star@gmail.com>
2017-07-20 00:33:55 +0800

    升级clojure

--- project.clj
+++ project.clj
@@ -1,21 +1,20 @@
 (defproject zhongzubao "0.1.0-SNAPSHOT"
             :description "FIXME: write description"
             :url "http://example.com/FIXME"
-
-            :dependencies [[org.clojure/clojure "1.7.0"]
+            :dependencies [[org.clojure/clojure "1.8.0"]
                            [lib-noir "0.9.5"]
-                           [ring-server "0.3.1"]
+                           [ring-server "0.4.0"]
                            [selmer "0.7.7"]
                            [clj-time "0.6.0"]
                            [digest "1.4.3"]
                            [ring-middleware-format "0.5.0"]
                            [environ "1.0.0"]
                            [http-kit "2.1.16"]
-                           [clj-http "1.0.1"]
+                           [clj-http "2.0.1"]
                            [org.clojure/core.match "0.3.0-alpha4"]
                            [compojure "1.4.0"]
                            [venantius/ultra "0.1.9"]
-                           [baotask "0.1.4-SNAPSHOT"]
+                           [baotask "0.1.2-SNAPSHOT"]
                            [org.clojure/data.json "0.2.4"]
                            [org.clojure/core.logic "0.8.10"]
                            [overtone/at-at "1.2.0"]
@@ -26,12 +25,11 @@
                            [prone "0.8.0"]
                            ^:source-dep [instaparse "1.3.6"]
                            ]
-            :uberjar-name "zhongzubao.jar"
+            :uberjar-name "crm.jar"
             :repl-options {:init-ns zhongzubao.repl}
-            :plugins [[org.clojars.bob-zhang/rui-mig "0.1.2"]
-                      ]
+            :plugins []
             :main zhongzubao.core
-            :user { :plugins [[lein-drip "0.1.1-SNAPSHOT"]]}
+            :user { :plugins [[org.clojars.bob-zhang/rui-mig "0.1.2"]]}
             :profiles
             {:uberjar {:omit-source true
                        :env {:production true}
